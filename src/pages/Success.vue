8d9a3ec05f76a56bf593dcd663f5c5cd2d308170
Brian Mwangi <brnmwas@gmail.com>
2020-02-01 19:09:05 +0300

    Change to success page

---
+++ src/pages/Success.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <div class="container-inner mx-auto py-16">
+      <h2 class="text-4xl font-bold mb-16">Thanks I will get back to you soon..</h2>
+      <g-image src="../../static/Success.png" />
+    </div>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Success'
+  }
+}
+</script>
